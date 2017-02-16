ce3725162bbd58b3b23ba188c8cbcde36b4e9148
Adam Lif <adamlnk@neb.rr.com>
2017-02-16 05:59:24 -0600

    Changing to use netbeans on vm

---
+++ osh/parse.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   parse.h
+ * Author: adam
+ *
+ * Created on February 16, 2017, 4:08 AM
+ */
+
+#ifndef PARSE_H
+#define PARSE_H
+
+class parse {
+public:
+    parse();
+    parse(const parse& orig);
+    virtual ~parse();
+private:
+
+};
+
+#endif /* PARSE_H */
+
