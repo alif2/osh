7d49d7fef75b9cd40b8ec7af71bae6f4fcd9a04a
Adam Lif <adamlnk@neb.rr.com>
2017-02-26 21:45:47 -0600

    Add primary functionality for pipes and redirects - single only

--- osh/osh.cpp
+++ osh/osh.cpp
@@ -1,5 +1,8 @@
 #include <cstdlib>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "command.h"
 #include "parse.h"
@@ -10,14 +13,19 @@ using namespace osh;
 int main(int argc, char** argv) {
     // Command structure
     Command *head = NULL;
-
+    
     // Bounded input loop to avoid infinite forks
-    for(int i = 0; i <= 25; i++) { 
+    for(int i = 0; i < 25; i++) { 
         int cmdcode = GetCommandChain(&head);
 
         if(cmdcode != status_success) {
-            printf("Error reading input");
+            fprintf(stderr, "Error reading input\n");
             exit(1);
+        }
+        
+        // Ignore blank lines
+        if(head->file == NULL || strlen(head->file) < 1) {
+            continue;
         }
         
         // Exit command signals end of input
@@ -25,59 +33,102 @@ int main(int argc, char** argv) {
             exit(0);
         }
         
-        if(head->prev->symbolType != (long int) NULL) {
-            if(head->prev->symbolType == ExecuteOnSuccess) {
-                if(head->prev->status != 0) {
-                    head->status = head->prev->status;
+        // Bounded parse loop to avoid infinite loop
+        int j = 0;
+        int fd[2];
+        while(head != NULL && j < 25) {    
+            if(head->prev != NULL && head->prev->symbolType != (long int) NULL) {
+                // Current process executes only on success of previous one
+                if(head->prev->symbolType == ExecuteOnSuccess) {
+                    if(head->prev->status != 0) {
+                        head->status = head->prev->status;
+                        break;
+                    }
+                }
+
+                // Current process executes only on failure of previous one
+                else if(head->prev->symbolType == ExecuteOnFailure) {
+                    if(head->prev->status == 0) {
+                        head->status = head->prev->status;
+                        break;
+                    }
                 }
             }
 
-            else if(head->prev->symbolType == ExecuteOnFailure) {
-                if(head->prev->status == 0) {
-                    head->status = head->prev->status;
+            if(head->symbolType == Pipe) {
+                if(pipe(fd) < 0) {
+                    fprintf(stderr, "Pipe failed\n");
+                    exit(1);
                 }
             }
-        }
-        
-        if(head->symbolType != (long int) NULL) {
-            if(head->symbolType == RedirectIn) {
-                
+
+            pid_t cpid = fork();
+            if(cpid < 0) {
+                fprintf(stderr, "Fork Failed\n");
+                exit(1);
             }
-            
-            else if(head->symbolType == RedirectOut) {
-                head->next->file;
+
+            // Child process
+            else if(cpid == 0) {
+                if(head->prev != NULL && head->prev->symbolType == Pipe) {
+                    if(dup2(fd[0], STDIN_FILENO) < 0) exit(1);
+                    close(fd[0]);
+                }
+
+                if(head->symbolType != Null) {
+                    char* redir = head->next->file;
+
+                    if(head->symbolType == RedirectIn) {
+                        head->inFileHandle = open(redir, O_RDONLY);
+                        if(dup2(head->inFileHandle, STDIN_FILENO) < 0) exit(1);
+                        close(head->inFileHandle);
+                    }
+
+                    else if(head->symbolType == RedirectOut) {
+                        head->outFileHandle = open(redir, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+                        if(dup2(head->outFileHandle, STDOUT_FILENO) < 0) exit(1);
+                        close(head->outFileHandle);
+                    }
+
+                    else if(head->symbolType == RedirectOutAppend) {
+                        head->outFileHandle = open(redir, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+                        if(dup2(head->outFileHandle, STDOUT_FILENO) < 0) exit(1);
+                        close(head->outFileHandle);
+                    }
+
+                    else if(head->symbolType == Pipe) {
+                        if(dup2(fd[1], STDOUT_FILENO) < 0) exit(1);
+                        close(fd[1]);
+                    }
+                }
+
+                execvp(head->file, head->arglist);
+                fprintf(stderr, "Exec Failed\n");
+                exit(1);
             }
-            
-            else if(head->symbolType == RedirectOutAppend) {
-                
+
+            // Parent process
+            else {
+                int status;
+                wait(&status);
+                head->status = status;
+
+                if(fd[1] > 1) close(fd[1]);
             }
-            
-            else if(head->symbolType == Pipe) {
-                
+
+            // Advance past filename for redirect tokens to avoid execution
+            if(head->symbolType == RedirectOut ||
+               head->symbolType == RedirectIn ||
+               head->symbolType == RedirectOutAppend) {
+
+                head = head->next;
             }
-        }
-        
-        //DumpCommandChain(head);
-        
-        pid_t cpid = fork();
-        if(cpid < 0) {
-            fprintf(stderr, "Fork Failed\n");
-            exit(2);
-        }
-        
-        else if(cpid == 0) {
-            execvp(head->file, head->arglist);
-            fprintf(stderr, "Exec Failed\n");
-            exit(3);
-        }
-        
-        else {
-            int status;
-            wait(&status);
-            head->status = status;
+
+            head = head->next;
+            j++;
         }
     }
-    
+
     DeleteCommandChain(head);
     return 0;
 }
